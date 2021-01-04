d951ef37c9f42472cc61ac6dac0947314038abec
ni3silra <ni3silra.sati@gmail.com>
2021-01-04 16:50:26 +0530

    Anagram Added and Utilities (String/Integer) checks
    Signed-off-by: ni3silra <ni3silra.sati@gmail.com>

---
+++ src/com/problemsolving/Anagrams.java
@@ -0,0 +1,36 @@
+package com.problemsolving;
+
+import java.util.Scanner;
+
+public class Anagrams {
+
+	static int makeAnagram(String a, String b) {
+		int[] alphabets = new int[26];
+		int diff = 0;
+
+		for (char c : a.toCharArray())
+			alphabets[c - 97]++;
+
+		for (char c : b.toCharArray())
+			alphabets[c - 97]--;
+
+		for (int i : alphabets)
+			if (i != 0)
+				diff++;
+
+		return diff;
+	}
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		String a = scanner.nextLine();
+		String b = scanner.nextLine();
+
+		int res = makeAnagram(a, b);
+		System.out.println(String.valueOf(res));
+
+		scanner.close();
+	}
+}
--- src/com/utilites/Utilites.java
+++ src/com/utilites/Utilites.java
@@ -3,24 +3,40 @@ package com.utilites;
 public class Utilites {
 
 	public static int compareTo(Object o1, Object o2) {
-		if (o1.equals(o2))
-			return 0;
-		else if ((o1 instanceof Number && o1 instanceof Number)
-				&& (Integer.parseInt(o1.toString()) > Integer.parseInt(o2.toString())))
-			return 1;
-		else if ((o1 instanceof String && o1 instanceof String) && (o1.toString().charAt(0) > o2.toString().charAt(0)))
-			return 1;
-		else
-			return -1;
+
+		try {
+			Integer obj1 = Integer.parseInt(o1.toString());
+			Integer obj2 = Integer.parseInt(o2.toString());
+
+			if (obj1 == obj2)
+				return 0;
+			else if (obj1 > obj2)
+				return 1;
+			else
+				return -1;
+
+		} catch (NumberFormatException e) {
+
+			if (o1.equals(o2))
+				return 0;
+			else if (o1.toString().charAt(0) > o2.toString().charAt(0))
+				return 1;
+			else
+				return -1;
+		}
+
 	}
 
 	public static boolean equals(Object o1, Object o2) {
-		Integer obj1 = Integer.parseInt(o1.toString());
-		Integer obj2 = Integer.parseInt(o2.toString());
 
-		if (obj1 instanceof Number && obj2 instanceof Number)
+		try {
+			Integer obj1 = Integer.parseInt(o1.toString());
+			Integer obj2 = Integer.parseInt(o2.toString());
 			return obj1 == obj2;
-		return (o1.toString().charAt(0) == o2.toString().charAt(0));
+		} catch (NumberFormatException e) {
+			return (o1.toString().charAt(0) == o2.toString().charAt(0));
+		}
+
 	}
 
 }
