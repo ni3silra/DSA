540bade16888a6a2d793dcfef2f1226b59cbf7bb
ni3silra <ni3silra.sati@gmail.com>
2021-01-09 13:03:25 +0530

    MaxMod Added in Problem-Solving
    Signed-off-by: ni3silra <ni3silra.sati@gmail.com>

---
+++ src/com/problemsolving/MaxMod.java
@@ -0,0 +1,29 @@
+package com.problemsolving;
+
+import java.util.*;
+
+public class MaxMod {
+
+	public static void main(String[] args) {
+		int[] array = {2, 6, 4};
+
+		ArrayList<Integer> list = new ArrayList<>();
+
+		for (int i : array)
+			list.add(i);
+		
+		System.out.println(new MaxMod().solve(list));
+	}
+
+	public int solve(ArrayList<Integer> A) {
+		int max = Integer.MIN_VALUE;
+
+		for (int i : A)
+			for (int j : A)
+				if (i % j > max)
+					max = i % j;
+
+		return max;
+	}
+
+}
