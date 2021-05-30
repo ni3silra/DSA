c903a1ea02364826a033328a4db1506735fec2e9
silra <silra@Ni3>
2021-05-30 22:48:30 +0530

    Diameter completion
    Signed-off-by: silra <silra@Ni3>

--- src/com/problemsolving/tree/Diameter.java
+++ src/com/problemsolving/tree/Diameter.java
@@ -3,23 +3,19 @@ package com.problemsolving.tree;
 public class Diameter {
 
 	public int solve(TreeNode A) {
-
 		if (A == null)
 			return -1;
 		int sizeLst = getHight(A.left);
 		int sizeRst = getHight(A.right);
-
 		return (int) Math.max(Math.max(sizeLst + sizeRst + 2, solve(A.left)), solve(A.right));
-
 	}
 
-	public Info getDia(TreeNode A) {
+	public Info treeUtils(TreeNode A) {
 		if (A == null)
 			return new Info(-1, -1);
-		int sizeLst = getHight(A.left);
-		int sizeRst = getHight(A.right);
-
-		return (int) Math.max(Math.max(sizeLst + sizeRst + 2, solve(A.left)), solve(A.right));
+		Info l = treeUtils(A.left);
+		Info r = treeUtils(A.right);
+		return new Info(Math.max(l.hight, r.hight), Math.max(Math.max(l.hight + r.hight + 2, l.diameter), r.diameter));
 	}
 
 	public int getHight(TreeNode node) {
